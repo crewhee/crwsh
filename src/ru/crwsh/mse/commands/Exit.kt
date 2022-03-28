14cecb21a333f091711a299503571f10aeb7312d
Crewhee <tony.sudov@gmail.com>
2022-03-28 06:04:06 +0300

    Fix warnings

--- src/ru/crwsh/mse/commands/Exit.kt
+++ src/ru/crwsh/mse/commands/Exit.kt
@@ -3,7 +3,10 @@ package ru.crwsh.mse.commands
 import kotlin.system.exitProcess
 
 class Exit : Command {
-    override fun Execute(args: List<String>, env: Map<String, String>): String? {
-        exitProcess(0);
+    override val type: String
+        get() = "command"
+
+    override fun Execute(args: List<String>, env: Map<String, String>): String {
+        exitProcess(0)
     }
 }
--- src/ru/crwsh/mse/shell/Shell.kt
+++ src/ru/crwsh/mse/shell/Shell.kt
@@ -3,7 +3,6 @@ package ru.crwsh.mse.shell
 import ru.crwsh.mse.commands.Command
 import ru.crwsh.mse.commands.CommandFactory
 import ru.crwsh.mse.commands.CommandType
-import ru.csh.mse.commands.*
 import java.io.PrintStream
 import java.util.Scanner
 import ru.crwsh.mse.parser.Parser
@@ -11,7 +10,7 @@ import ru.crwsh.mse.parser.Parser
 class Shell (
     private var env : MutableMap<String, String> = mutableMapOf(),
     private val parser : Parser = Parser(),
-    private val input_stream : Scanner = Scanner(java.lang.System.`in`),
+    private val input_stream : Scanner = Scanner(System.`in`),
     private val output_stream : PrintStream = PrintStream(System.out),
     private var commandGetter : MutableMap<String, Command> = mutableMapOf(),
     commandFactory: CommandFactory = CommandFactory()
@@ -24,16 +23,16 @@ class Shell (
 
     fun run() {
         while (true) {
-            var p_res = parser.Parse(input_stream, env)
-            var res : String?
-            if (p_res.commandName in commandGetter) {
-                res = commandGetter[p_res.commandName]?.Execute(p_res.args, env)
-
-                output_stream.println(res)
-            }
-            else {
-                output_stream.println("csh: command not found: " + p_res.commandName)
-            }
+//            var p_res = parser.Parse(input_stream, env)
+//            var res : String?
+//            if (p_res.commandName in commandGetter) {
+//                res = commandGetter[p_res.commandName]?.Execute(p_res.args, env)
+//
+//                output_stream.println(res)
+//            }
+//            else {
+//                output_stream.println("csh: command not found: " + p_res.commandName)
+//            }
             output_stream.println("sh >")
         }
     }
